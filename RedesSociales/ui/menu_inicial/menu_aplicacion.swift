9d2706c11797b0e2ec34518c5bddb97f9d97b739
alumno <alumno@pc-czarate.uacj.mx>
2025-03-31 09:38:56 -0600

    lunes

---
+++ RedesSociales/ui/menu_inicial/menu_aplicacion.swift
@@ -0,0 +1,18 @@
+//
+//  menu_aplicacion.swift
+//  RedesSociales
+//
+//  Created by alumno on 3/31/25.
+//
+
+import SwiftUI
+
+struct menu_aplicacion: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    menu_aplicacion()
+}
